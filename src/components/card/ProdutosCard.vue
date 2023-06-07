e5d21704becfba524da681c0b043a3d83f7f22a2
Paola Sensato <paolasensato@jera.com.br>
2023-06-07 12:58:38 -0300

    Alinha cards

--- src/components/card/ProdutosCard.vue
+++ src/components/card/ProdutosCard.vue
@@ -1,6 +1,6 @@
 <template>
   <RouterLink :to="'/produtos/' + id" class="link">
-    <div class="card shadowh-100">
+    <div class="card shadowh-100 h-100">
       <img
         :src="'http://localhost/admin/fotos/' + imagem + 'p.jpg'"
         :alt="produto"
@@ -8,7 +8,7 @@
       />
       <div class="card-body">
         <h5 class="card-title text-break">{{ produto }}</h5>
-        <p class="card-text">{{ valor }}</p>
+        <p class="card-text text-center">{{ valor }}</p>
       </div>
     </div>
   </RouterLink>
@@ -31,6 +31,7 @@ const { id, produto, valor, imagem } = props.data
 .card {
   border: 0;  
   transition: 0.5s;
+  align-items: center;
 }
 
 .card:hover {
@@ -39,7 +40,8 @@ const { id, produto, valor, imagem } = props.data
 
 .card-img {
   border-radius: 10px 10px;
-  
+  border: none !important;
+  width: 50%;
 }
 .card-text {
   color: #ef5fa5;
