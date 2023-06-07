b11070dfa77686965170f87a09c263dd3c15ce9e
Paola Sensato <paolasensato@jera.com.br>
2023-06-07 16:35:44 -0300

    Altera card de Produto

--- src/components/card/ProdutoCard.vue
+++ src/components/card/ProdutoCard.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="row h-100">
+    <h3 class="title">
+      <strong>{{ data.produto }}</strong>
+    </h3>
     <div class="col-12 col-md-5">
       <img
         :src="'http://localhost/admin/fotos/' + data.imagem + 'p.jpg'"
-        class="w-100"
+        class="w-100 image"
         :alt="data.produto"
       />
     </div>
-    <div class="col-12 col-md-7 text-center py-5">
-      <h1 class="title">
-        <strong>{{ data.produto }}</strong>
-      </h1>
+    <div class="col-12 col-md-7 py-5">
       <p v-html="data.descricao"></p>
     </div>
   </div>
@@ -28,7 +28,12 @@ const props = defineProps({
 }
 
 .title {
+  padding-top: 10px;
   color: #444444;
+}
+
+.image {
+  margin-top: 5rem;
 }
 @media screen and (max-width: 768px) {
   .row {
